5f763b565a4a849387fec5d8b19b6711c18623e0
yeongjin <sovt123@naver.com>
2025-10-31 18:02:59 +0900

    251031+

---
+++ Electrical Circuit_Demo/Assets/Scripts/2DScript/VirtualJunction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 이 컴포넌트는 해당 Junction이
+/// 부품 삭제로 인해 생성된 '가상 접점'임을 나타내는 마커입니다.
+/// SymbolController가 이 마커를 찾아 스냅 연결을 시도합니다.
+/// </summary>
+public class VirtualJunction : MonoBehaviour
+{
+    // 내용이 필요 없습니다. 마커로만 사용됩니다.
+}
